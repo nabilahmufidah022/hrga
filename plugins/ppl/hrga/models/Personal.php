ad65fba847dc499b869ef6541a7f2add6c8a2df7
Keisha Syifa <keisha.ekza@gmail.com>
2025-05-17 17:40:10 +0700

    Perubahan Datadiri

--- plugins/ppl/hrga/models/Personal.php
+++ plugins/ppl/hrga/models/Personal.php
@@ -3,6 +3,7 @@
 #use Winter\Storm\Database\Model;
 use Model;
 use Ppl\Hrga\Models\Division as MoDivisi;
+use Ppl\Hrga\Models\Position as MoJabatan;
 
 /**
  * personal Model
@@ -109,16 +110,32 @@ class Personal extends Model
         // return $Divisi_id;
     }
 
-      public function getJabatanIdOptions($value, $formData)
+    public function getJabatanIdOptions($value, $formData)
     {
-        // $DivisiData = MoDivisi::get(['id','nama_divisi', 'kode_divisi'])->toArray();
-        // $Divisi = [];
-        // foreach($DivisiData as $value) {
-        //     $Divisi[$value['id']] = $value['kode_divisi']; 
-        // }
-        // return $Divisi;
+        $JabatanData = MoJabatan::get(['id','nama_jabatan'])->toArray();
+        $Jabatan = [];
+        foreach($JabatanData as $value) {
+            $Jabatan[$value['id']] = $value['nama_jabatan']; 
+        }
+        return $Jabatan;
 
-        $Divisi_id = MoDivisi::selectRaw("*, concat(kode_divisi,' - ', nama_divisi) as divisi")->lists('divisi', 'id');
-        return $Divisi_id;
+        // $Divisi_id = MoDivisi::selectRaw("*, concat(kode_divisi,' - ', nama_divisi) as divisi")->lists('divisi', 'id');
+        // return $Divisi_id;
     }
+
+    // public function getKodeJabatanAttribute($value)
+    // {
+    //     // $divisi = DB::table('merapat_divisi')->join()->where('divisi_id', $id);
+    //     $JabatanData = MoJabatan::where('id','=', $this->jabatan_id)->first();
+    //     // dd($DivisiData);
+    //     $nama_jabatan = $JabatanData->nama_jabatan;
+    //     // $kode_divisi = $DivisiData->kode_divisi;
+
+    //     $dataJabatan = $nama_jabatan;
+
+    //     return $dataJabatan;
+    //     // $Divisi_id = MoDivisi::selectRaw("*, concat(kode_divisi,' - ', nama_divisi) as divisi")->where($this->divisi_id, '=', 'id');
+    //     // // dd($Divisi_id);
+    //     // return $Divisi_id;
+    // }
 }
